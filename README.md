0e5694e2f0ab06ed183f939cf6896a26582569e6
Melissa Smith <melissasmith@Melissas-MacBook-Pro.local>
2016-06-24 10:42:46 -0700

    add ava for testing, babel preset

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 Boilerplate for React-Redux apps with the following features:
 * React
 * React-Redux
+* React Router
 * Webpack & Hot Loading
 * Babel
 * React Bootstrap for quick prototyping
+* Ava for testing
--- app/index.js
+++ app/index.js
@@ -1,18 +1,11 @@
-import './main.css';
 import React from 'react';
 import { render } from 'react-dom';
 import { createStore } from 'redux';
-// makes the containers aware of the store
 import { Provider } from 'react-redux';
-import todo from './store/reducers/todo.js';
-import { TodoListContainer } from './containers/Todo.js';
-import NoteContainer from './containers/Note.js';
 
-import { List, Map } from 'immutable';
-// import App from './components/App.js';
-const store = createStore(todo);
+const store = createStore();
 
-render(<div><NoteContainer />
+render(<div>
 <Provider store={store}>
-  <TodoListContainer />
+
 </Provider></div>,document.getElementById('app'));
